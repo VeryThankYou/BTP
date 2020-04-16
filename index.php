9261b8ecb89b5867694122d2b489a5ce844563c3
Vilijam <37106752+Vilijam@users.noreply.github.com>
2020-04-16 15:15:59 +0200

    gjort ting

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="maincss.css">
+    <link rel="stylesheet" href="styling/maincss.css">
     <title>Balduvian Trading Post</title>
 </head>
 
