729f4483575932d414de3dba0a7417f55e464f49
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2020-05-13 08:51:58 +0200

    glemte noget

---
+++ mutual.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+//This file has our connection to our database
+include('config.php');
+
+//Check if we have a session called email. This way we block users from changing the url and trying to skip login.
+if(!isset($_SESSION['id'])){
+  header('location: index.php');  
+}
+
+function userID($email, $conn){
+  $sql = "SELECT id FROM user WHERE email='$email';";
+  $result = $conn->query($sql);
+  $fetch = $result;
+  $row = mysqli_fetch_assoc($fetch);
+  return $row['id'];
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="styling/maincss.css">
+  <title>Balduvian Trading Post</title>
+</head>
+
+<body>
+
+  <div class="header">
+    <div class="header_left">
+      <form method="POST">
+        <input type='submit' name='back' value='BTP'>
+      </form>
+    </div>
+
+    <div class="header_right">
+      <a href="index.php">
+
+        <p>Logout</p>
+      </a>
+    </div>
+  </div>
+
+  <div style="clear:both;"></div>
+
+
+</body>
+</html>
